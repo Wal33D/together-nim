240104928f34c2310188adc6f04812384cc39124
Waleed Judah <aquataze@yahoo.com>
2026-03-19 04:54:20 -0400

    Fix test_character to match actual character dimensions

--- tests/test_character.nim
+++ tests/test_character.nim
@@ -3,42 +3,44 @@ import "../src/entities/character"
 import "../src/constants"
 
 suite "character":
-  test "pip is 30x30 with doubleJump":
+  test "pip with doubleJump":
     let c = newCharacter("pip")
     check c.id == "pip"
-    check c.width == 30
-    check c.height == 30
+    check c.width == 24
+    check c.height == 24
+    check c.color == PIP_COLOR
     check c.ability == doubleJump
 
-  test "luca is 25x40 with floatAbility":
+  test "luca with floatAbility":
     let c = newCharacter("luca")
     check c.id == "luca"
-    check c.width == 25
-    check c.height == 40
+    check c.width == 28
+    check c.height == 28
     check c.ability == floatAbility
 
-  test "bruno is 50x50 with heavy":
+  test "bruno with heavy":
     let c = newCharacter("bruno")
-    check c.width == 50
-    check c.height == 50
+    check c.width == 40
+    check c.height == 40
     check c.ability == heavy
 
-  test "cara is 20x45 with wallJump":
+  test "cara with wallJump":
     let c = newCharacter("cara")
     check c.width == 20
-    check c.height == 45
+    check c.height == 20
     check c.ability == wallJump
 
-  test "felix is 35x35 with coyoteTime":
+  test "felix with coyoteTime":
     let c = newCharacter("felix")
-    check c.width == 35
-    check c.height == 35
+    check c.width == 20
+    check c.height == 50
     check c.ability == coyoteTime
 
-  test "ivy is 28x42 with gracefulFall":
+  test "ivy with gracefulFall":
     let c = newCharacter("ivy")
     check c.width == 28
-    check c.height == 42
+    check c.height == 28
+    check c.color == IVY_COLOR
     check c.ability == gracefulFall
 
   test "default fields on new character":
